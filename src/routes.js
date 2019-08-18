0c31fac65183ce9075b839cdbfb60dc16bf75cd2
roney-mendes <roneymendes1@gmail.com>
2019-08-18 12:04:26 -0300

    Colocando a pasta node_modules no git ignore

--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,13 @@ const routes = express.Router();
 routes.get('/', (req, res) => {
     //return res.send('Hello World');
     //return res.json(`Hello ${req.query.name}`);
-    return res.json({message:`Olá ${req.query.name}`});
+    return res.json({message:`Olá ${req.query.name}, tudo bem com você?`});
 });
 
 routes.post('/devs', (req, res) =>{
 
     console.log(req.body);
-    return res.json({ok: true});
+    return res.json(req.body);
 })
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const routes = require('./routes');
 
 const server = express();
 
+server.use(express.json);
+
 server.use(routes);
 
 server.listen(3333);
